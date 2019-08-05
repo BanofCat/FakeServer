e10b725da724e8f00dab094bdb519d8a6bee3d17
peng_yz <634842352@qq.com>
2019-08-05 17:20:29 +0800

    save 20190805

--- modules/DeviceServer/include/device_server.h
+++ modules/DeviceServer/include/device_server.h
@@ -4,14 +4,30 @@
 namespace fake
 {
 
+#define CYCLE_TIME_US 2000 // 2000us
+#define CYCLE_TIME_NS 1000 * CYCLE_TIME_US // 2000us = 2000 000 ns
+#define WAIT_STOP_TIMES 1000    // wait to stop max times, when exceed it, raise exception
+
+enum ErrorCode {
+    OK      = 0,
+    RUN_ERR = -1,
+    TIMEOUT = -2,
+};
 
 class DeviceServer
 {
+private:
+    bool keep_running;  // if need to keep running
+    bool is_running;    // if is running now
+    void addTimeNS(struct timespec& t_obj, unsigned int nano_time);   
+    void setRunning(bool new_state);
 
 public:
-
+    bool keepRunning();
+    void setKeepRunning(bool new_state);
+    bool isRunning();
     int run();
-
+    int stop();
 
 
 };  // !class DeviceServer
--- modules/DeviceServer/src/device_server.cpp
+++ modules/DeviceServer/src/device_server.cpp
@@ -1,27 +1,71 @@
 #include "device_server.h"
 namespace fake 
 {
+/* private */
 
-device_server::
+void device_server::
 init()
 {
 
 }
 
+void addTimeNS(struct timespec& t_obj, unsigned int nano_time)
+{
+	t_obj.tv_nsec += nano_time;
+	while(t_obj.tv_nsec >= 10E9) {
+		t_obj.tv_nsec -= 10E9;
+		t_obj.tv_sec++;
+	}
+}
+
+
+/* public */
+
+int device_server::
+stop(int timeout_us)
+{
+    unsigned int stop_counter = 0;
+    this->setKeepRunning(false);
+    while(this->isRunning()) {
+        usleep(1000);   // sleep 1000us
+        stop_counter++;
+        if (stop_counter > WAIT_STOP_TIMES) {
+            throw std::exception('wait to stop timeout');
+        }
+    }
+    return OK;
+}
 
 int device_server::
 run()
 {
+    fprintf(stderr, "start to run device server!\n");
+    int ret = 0;
     try {
+        struct timespec time_obj;
+        this->setRunning(true);
+        while(this->keepRunning()) {
+            clock_gettime(CLOCK_MONOTONIC, &time_obj);  // store current time
+            this->addTimeNS(time_obj, CYCLE_TIME_NS);   // calculate the target time
 
-        rc_obj.dev_write();
-        
-        rc_obj.dev_read();
+            gui_obj.dev_read();     // update gui data before write to rcs
 
+            rc_obj.dev_write();
 
-    } catch (Exception e) {
+            rc_obj.dev_read();      // update rcs data before write to gui
 
+            gui_obj.dev_write();
+
+            clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &time_obj, NULL);   // sleep until the target time
+        }
+
+    } catch (std::exception e) {
+        fprintf(stderr, "catch error: %s\n", e.what());
+        ret = RUN_ERR;
     }
+    this->setRunning(false);
+    fprintf(stderr, "at the end of device server! \n");
+    return ret;
 }
 
 
